a062813382e8b30bac9ba5dfc0e43d708057bf77
Ghostmaysam <116579333+Ghostmaysam1@users.noreply.github.com>
2025-05-19 21:59:36 +0330

    feat: add `Navbar&Footer` layout (#4)
    * feat: add Navbar to Navbar&Footer
    
    * chore: folder struct change
    
    * feat: add Footer to Navbar&Footer layout

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
+import NavbarFooter from './layouts/Navbar&Footer/Navbar&Footer.vue';
 </script>
 
 <template>
-  <RouterView />
+  <NavbarFooter>
+    <RouterView />
+  </NavbarFooter>
 </template>
