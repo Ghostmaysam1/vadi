bf40d4bb5e797fd495814aefebf5e89a8e3578a4
Ghostmaysam <116579333+Ghostmaysam1@users.noreply.github.com>
2025-05-19 11:12:32 +0330

    feat: add views to project (#3)

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,16 @@
 import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../view/HomeView.vue";
 
 
 
 const router = createRouter({
     history: createWebHistory('/'),
     routes: [
-        
+        {
+            name: 'Home',
+            path: '/',
+            component: HomeView
+        }
     ]
 })
 
