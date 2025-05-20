f998a4064e0a9ed07ab3dd62795eaa989f39973c
Ghostmaysam <116579333+Ghostmaysam1@users.noreply.github.com>
2025-05-20 23:09:11 +0330

    feat: add services section

---
+++ src/view/Home/Services/Services.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import ServiceBox from './components/ServiceBox/ServiceBox.vue';
+</script>
+
+<template>
+    <div class="w-full pt-5 flex flex-col items-center">
+        <h3 class="w-fit text-center px-10 border border-black/10 shadow-lg py-2 rounded-full text-white bg-linear-to-br/oklab from-[#526168]/60 to-[#87675a]/60 text-4xl font-[600]">خدمات</h3>
+        <div class="w-full h-[500px] flex justify-evenly items-center px-5">
+            <ServiceBox title="خدمات اجرا" src="/images/xa.png" />
+            <ServiceBox title="مشاوره حضوری" src="/images/xb.png" />
+            <ServiceBox title="VIP خدمات" src="/images/xc.png" />
+        </div>
+    </div>
+</template>
---
+++ src/view/Home/Services/components/ServiceBox/ServiceBox.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const {src, title} = defineProps<{
+    src: `/images/${string}`,
+    title: string
+}>();
+</script>
+
+<template>
+    <div :style="{backgroundImage: `url(${src})`}" class="bg-cover w-1/5 h-[70%] rounded-md flex justify-center items-center backdrop-grayscale-25 hover:shadow-2xl shadow-amber-600/30 opacity-80 hover:opacity-100 transition-all duration-500">
+        <p class="w-fit cursor-pointer text-center px-10 border border-black/10 shadow-lg py-2 rounded-full text-white bg-linear-to-br/oklab from-[#526168]/60 to-[#87675a]/60 backdrop-blur-sm text-lg hover:font-bold hover:scale-110 transition-all duration-75 font-[400]" v-text="title"></p>
+    </div>
+</template>
