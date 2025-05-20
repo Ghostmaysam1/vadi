bd4977f01adb7d8f96fdfa635f231d76cd10ffa5
Ghostmaysam <116579333+Ghostmaysam1@users.noreply.github.com>
2025-05-20 22:15:39 +0330

    feat: add services section (#6)

--- src/view/HomeView.vue
+++ src/view/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import Header from './Home/Header/Header.vue';
+import Services from './Home/Services/Services.vue';
 </script>
 
 <template>
     <div class="w-full">
         <Header />
+        <Services />
     </div>
 </template>
