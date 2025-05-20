708ace9b5527a79ff5db238c92fca6977fb49354
Ghostmaysam <116579333+Ghostmaysam1@users.noreply.github.com>
2025-05-20 23:25:54 +0330

    fix: fix responsive for mobile

--- src/view/Home/Services/Services.vue
+++ src/view/Home/Services/Services.vue
@@ -5,7 +5,7 @@ import ServiceBox from './components/ServiceBox/ServiceBox.vue';
 <template>
     <div class="w-full pt-5 flex flex-col items-center">
         <h3 class="w-fit text-center px-10 border border-black/10 shadow-lg py-2 rounded-full text-white bg-linear-to-br/oklab from-[#526168]/60 to-[#87675a]/60 text-4xl font-[600]">خدمات</h3>
-        <div class="w-full h-[500px] flex justify-evenly items-center px-5">
+        <div class="w-full md:h-[500px] flex gap-2 flex-col md:flex-row justify-evenly items-center px-5 mt-6">
             <ServiceBox title="خدمات اجرا" src="/images/xa.png" />
             <ServiceBox title="مشاوره حضوری" src="/images/xb.png" />
             <ServiceBox title="VIP خدمات" src="/images/xc.png" />
