8b65407eb8ebfcf4a6d70e66d1dacebb064be303
Ghostmaysam <116579333+Ghostmaysam1@users.noreply.github.com>
2025-05-21 16:16:21 +0330

    feat: add team section

---
+++ src/view/Home/Team/Member/Member.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const { slide } = defineProps<{
+  slide: {
+    img: string;
+    name: string;
+    title: string;
+  };
+}>();
+</script>
+
+<template>
+  <div class="border-b rounded-lg w-2/3 md:w-full p-2 flex h-full flex-col items-center">
+    <div class="h-2/3">
+      <img :src="slide.img" class="h-full rounded-sm" />
+    </div>
+    <div class="flex flex-col items-center justify-evenly h-full">
+      <p class="font-bold text-lg text-right">{{ slide.name }}</p>
+      <p class="font-medium text-md">{{ slide.title }}</p>
+      <button>رزومه</button>
+    </div>
+  </div>
+</template>
--- src/view/HomeView.vue
+++ src/view/HomeView.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import Header from './Home/Header/Header.vue';
 import Services from './Home/Services/Services.vue';
+import Team from './Home/Team/Team.vue';
 </script>
 
 <template>
     <div class="w-full">
         <Header />
         <Services />
+        <Team />
     </div>
 </template>
