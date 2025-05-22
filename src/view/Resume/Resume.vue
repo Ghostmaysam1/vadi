289ef0622ec5951835e5a5a41ba4c2523252faef
Ghostmaysam <116579333+Ghostmaysam1@users.noreply.github.com>
2025-05-22 07:49:40 +0330

    feat: add Resume page

---
+++ src/view/Resume/Resume.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import View from './components/View/View.vue';
+</script>
+
+<template>
+    <div class="min-h-screen bg-gradient-to-b from-[#f8f5f2] to-white flex items-center justify-center p-4">
+        <View />
+    </div>
+</template>
---
+++ src/view/ResumeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+    import Resume from './Resume/Resume.vue';
+</script>
+
+<template>
+    <div class="w-full min-h-[100vh]">
+        <Resume />
+    </div>
+</template>
